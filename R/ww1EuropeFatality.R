a0a0f78680eb37c46685109af920dcec0914dab5
lsgould <lsgould@smith.edu>
2025-12-10 17:57:07 -0500

    more

---
+++ R/ww1EuropeFatality.R
@@ -0,0 +1,7 @@
+#' European Fatalities in World War I
+#'
+#' Data set recording the death toll, both military and civilian, for all European countries involved with World War I.
+#'
+#'
+#' @format A data frame of columns 'NAME,' 'DEATHS_TOTAL,' 'DEATHS_MILITARY', and 'DEATHS_CIV'
+"ww1EuropeFatality"
