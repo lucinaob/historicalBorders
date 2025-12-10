6e2239b2fc643ad6dc6dc31ed8c96f87f44267bb
lsgould <lsgould@smith.edu>
2025-12-10 17:55:09 -0500

    vignette

--- R/mapCustom.R
+++ R/mapCustom.R
@@ -8,22 +8,47 @@
 #' @param year The year the user wants to see reflected by borders.
 #' @param lat The latitude of the area the user wants to map.
 #' @param long The longitude of the area the user wants to map.
-#' @param dataset The dataset of historical borders.
+#' @param dataset Additional data to map over historical borders.
+#' @param aesthetics The aesthetic features (ex. fill, color) to map onto the borders
 #'
 #' @return A custom map of the latitude and longitudes specified by R.
 #' @export
 #'
 #' @examples
-#' mapCustom(1800, c(43.29, 57.10), c(74.21, 100.7))
-#' mapCustom(1820, c(48.85, 55.5), c(2.35, 10.8))
+#' mapCustom(1800, c(19.5, 45.4), c(25.3, 41.2))
+#' mapCustom(1900, c(62.7, 130.6), c(-3.3, 44.2), population1900, aes(fill = POP))
 #'
 mapCustom <- function(year, lat, long, dataset = NULL, aesthetics = NULL){
-
-  data <- historicalBorders::world
 
   if(length(lat) != 2 || length(long) != 2){
     stop("Please enter 2 pairs of latitude and longitude values!")
   }
+
+  if(lat[2] < lat[1]){
+    lat <- rev(lat)
+  }
+
+  if(long[2] < long[1]){
+    long <- rev(long)
+  }
+
+  if(!is.numeric(year)){
+    stop("Year must be numeric (ex. 1800, 1925)")
+  }
+
+  if(length(year) > 1){
+    stop("Only one year can be mapped at a time.")
+  }
+
+  if(!is.null(aesthetics) && is.null(dataset)){
+    stop("You must specify an additional dataset to specify aesthetics")
+  }
+
+  if(!is.null(dataset) && is.null(aesthetics)){
+    stop("To visualize additional data, you must specify an aesthetic mapping")
+  }
+
+  data <- historicalBorders::world
 
   options <- unique(data$year)
 
