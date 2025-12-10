18025ac6d6364624a066d34fc0549da2b14cf9d3
lsgould <lsgould@smith.edu>
2025-12-10 17:02:10 -0500

    test mapworld

--- tests/testthat/test-mapworld.R
+++ tests/testthat/test-mapworld.R
@@ -1,3 +1,18 @@
 test_that("input 'year' is numeric",{
-  expect_error(mapworld("Eighteen-hundred and five"))
+  expect_error(mapWorld("Eighteen-hundred and five"))
+})
+
+test_that("mapWorld returns a ggplot object for valid input", {
+  world_year <- unique(historicalBorders::world$year)[1]
+  plot <- mapWorld(world_year)
+  expect_s3_class(plot, "ggplot")
+})
+
+test_that("mapWorld does not modify the world dataset", {
+  original <- historicalBorders::world
+  year <- unique(original$year)[1]
+
+  mapWorld(year)
+
+  expect_identical(original, historicalBorders::world)
 })
